9c83c7559f57fc98e98a3360a4164f02eb51ffaf
junho <2171168@hansung.ac.kr>
2026-01-10 02:47:28 +0900

    docs: api 스웨거 문서화

--- src/main/java/navik/domain/users/controller/UserControllerDocs.java
+++ src/main/java/navik/domain/users/controller/UserControllerDocs.java
@@ -1,14 +1,19 @@
 package navik.domain.users.controller;
 
+import jakarta.validation.Valid;
+import navik.domain.users.dto.UserRequestDTO;
 import navik.domain.users.dto.UserResponseDTO;
 import navik.global.apiPayload.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import navik.global.apiPayload.code.status.GeneralSuccessCode;
 import navik.global.auth.annotation.AuthUser;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "User", description = "유저 관련 API")
 public interface UserControllerDocs {
@@ -17,4 +22,9 @@ public interface UserControllerDocs {
 
 	@Operation(summary = "사용자 조회", description = "특정 사용자의 정보를 가져옵니다")
 	ApiResponse<UserResponseDTO.UserInfoDTO> getUser(@PathVariable Long userId);
+
+	@Operation(summary = "사용자 초기 정보 등록", description = "사용자 상태가 `PENDING`인 경우, 이름, 닉네임, 직무 등 필수 정보를 입력하여 가입을 완료합니다.")
+	ApiResponse<UserResponseDTO.BasicInfoDto> register(@AuthUser Long userId, @RequestBody @Valid UserRequestDTO.BasicInfoDto req);
+
+
 }
